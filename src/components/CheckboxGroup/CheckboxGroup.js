168f9f94f0315e847a04c07c84b4c6ee4dc82b2c
YaSlovar_work <stanko.tsep@yandex.ru>
2022-11-10 16:25:31 +0700

    some

---
+++ src/components/CheckboxGroup/CheckboxGroup.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import './CheckboxGroup.css';
+
+function CheckboxGroup({selected}) {
+    
+
+    return (
+        <>
+        </>
+    );
+}
+
+export default CheckboxGroup;
